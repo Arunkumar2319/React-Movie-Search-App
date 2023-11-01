201269eb713501ff1722c79244fd1d43932d48e9
Arun <arun.kumar@perfomatix.com>
2023-11-01 19:03:49 +0530

    movie_app - initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import SearchBox from "./SearchBox";
+
+const NavBar = (props) => {
+    return (
+        <div className='row d-flex App'>
+            <div className="mt-3 navbar-header">
+                <h3><b>Movie App</b></h3>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
