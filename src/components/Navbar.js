ddf519860c4f5bd17f3a17a870eefdec6a17ead8
Arun <arun.kumar@perfomatix.com>
2023-11-14 09:22:23 +0530

    UI updates responsiveness - initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ import { setTheme } from '../features/themeSwitchSlice';
 // import darkTheme from '../images/dark-theme.svg';
 import lightTheme from '../images/light-theme.svg'
 import userLogo from '../images/userLogo.svg';
-// import favourites from '../images/favourites.svg'
-
+import favourites from '../images/favourites.svg'
+// import menuIcon from '../images/hamburger_menu.svg'
 
 import '../styles/NavBar.css';
 import Profile from "./Profile";
@@ -22,7 +22,8 @@ const NavBar = (props) => {
     }
 
     const [credentialDetails] = useState(useSelector((Store) => Store.credentials.credentialDetails));
-    const [openProfileSettings, setProfileSettings] = useState(false)
+    const [openProfileSettings, setProfileSettings] = useState(false);
+    const screenWidth = window.innerWidth;
 
     const openUserProfileSettings = () => {
         setProfileSettings(true)
@@ -57,6 +58,14 @@ const NavBar = (props) => {
         }
     }
 
+    const openNav = () => {
+        document.getElementById("mySidepanel").style.width = "176px";
+    }
+
+    const closeNav = () => {
+        document.getElementById("mySidepanel").style.width = "0px";
+    }
+
     return (
         <div className='row d-flex App' style={stylesForTheme}>
             <div className="mt-3 mb-2 navbar-header">
@@ -72,26 +81,50 @@ const NavBar = (props) => {
                     </input>
                 </div>
             </div>
-            {credentialDetails != null ? (
-                <div onClick={changeTheme} title="change theme">
-                    <img className="theme-icon-style" src={lightTheme} alt='light theme' />
-                </div>
-            ) : null}
-            {credentialDetails != null ? (
-                <div className='favourites mt-3' title='Favourites' onClick={(event) => navigateToFavouritesPage(event)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 512 512" fill='red'>
-                        <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-                    </svg>
-                </div>
-            ) : null}
-            <div className='userLogo mt-3'  onClick={openUserProfileSettings}>
-                <img className='userIcon' src={userLogo} alt="user-logo" style={{color: "black"}}/>
-            </div>
-            {openProfileSettings ? (
-                <div className='profile' onClick={closeUserProfileSettings}>
-                    <Profile />
-                </div>
-            ) : null}
+            {screenWidth > 426 ? (
+                <>
+                    {credentialDetails != null ? (
+                        <div onClick={changeTheme} title="change theme">
+                            <img className="theme-icon-style" src={lightTheme} alt='light theme' />
+                        </div>
+                    ) : null}
+                    {credentialDetails != null ? (
+                        <div className='favourites mt-3' title='Favourites' onClick={(event) => navigateToFavouritesPage(event)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 512 512" fill='red'>
+                                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+                            </svg>
+                        </div>
+                    ) : null}
+                    <div className='userLogo mt-3' onClick={openUserProfileSettings}>
+                        <img className='userIcon' src={userLogo} alt="user-logo" style={{ color: "black" }} />
+                    </div>
+                    {openProfileSettings ? (
+                        <div className='profile' onClick={closeUserProfileSettings}>
+                            <Profile />
+                        </div>
+                    ) : null}
+                </>
+            ) : (
+                <>
+                    <div onClick={openNav}>
+                        <button className="hamburgerMenu" >☰</button>
+                    </div>
+                    <div id="mySidepanel" class="sidepanel">
+                        <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>×</a>
+                        {credentialDetails != null ? (
+                            <>
+                                <a href="/favourites" className="mt-2"><img className="m-1" src={favourites} alt="favourite" style={{width: "6vw"}}/>Favourites</a>
+                                <a href="/"><img className="m-1" src={userLogo} alt="login" style={{width: "6vw"}}/>Logout</a>                        
+                            </>
+                        ): (
+                            <>
+                                {/* <a href="javascript:void(0)">Profile</a>                         */}
+                                <a href="/login"><img className="m-1" src={userLogo} alt="login" style={{width: "6vw"}}/>Login</a>                        
+                            </>
+                        )}
+                    </div>
+                </>
+            )}
         </div>
     )
 }
